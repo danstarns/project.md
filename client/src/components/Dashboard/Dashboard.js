634d8bc64e641dbf7e78e233837ad63dacbb0dfd
Daniel Starns <danielstarns@hotmail.com>
2020-05-19 18:24:40 +0100

    feat: add some content to the dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
+import { Row, Col, Jumbotron, Button, Alert } from "react-bootstrap";
 
 const Query = gql`
   {
@@ -16,7 +17,42 @@ function Dashboard() {
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
 
-  return <h1>Hi From Dashboard, Welcome {data.me.username}</h1>;
+  return (
+    <>
+      <Row className="mt-3">
+        <Col xs={12} s={12} lg={12}>
+          <Alert show variant="success">
+            <Alert.Heading>How's it going {data.me.username}?!</Alert.Heading>
+            <p>Welcome Back!</p>
+          </Alert>
+        </Col>
+        <Col xs={12} s={12} lg={6}>
+          <Jumbotron>
+            <h1>Projects</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Enter</Button>
+            </p>
+          </Jumbotron>
+        </Col>
+        <Col xs={12} s={12} lg={6}>
+          <Jumbotron>
+            <h1>Organizations</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Enter</Button>
+            </p>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Dashboard;
