beab29ad47fe692b84ae7af059c92e5bc94d7462
Daniel Starns <danielstarns@hotmail.com>
2020-05-19 17:02:00 +0100

    feat: refactor logout page to show banner

--- client/src/components/Auth/Logout/Logout.js
+++ client/src/components/Auth/Logout/Logout.js
@@ -1,20 +1,24 @@
 import React, { useContext, useEffect } from "react";
+import { Alert } from "react-bootstrap";
 import { AuthContext } from "../../../contexts/index.js";
 
+/**
+ * Use window.redirect if GraphQL returns 401.
+ * GraphQL context is wrapping Auth context.
+ */
 function Logout() {
   const { logout, isLoggedIn } = useContext(AuthContext.Context);
 
   useEffect(() => {
-    logout();
+    setTimeout(() => {
+      logout();
+    }, 1000); // time for banner to display
   }, [isLoggedIn, logout]);
 
   return (
-    <>
-      {/**
-       * Use window.redirect if GraphQL returns 401.
-       * GraphQL context is wrapping Auth context.
-       */}
-    </>
+    <Alert variant="warning" className="mt-3">
+      Logged Out!
+    </Alert>
   );
 }
 
