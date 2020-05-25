f8f038d597ebffef85b4e0707c00adfd08f93bc8
Daniel Starns <danielstarns@hotmail.com>
2020-05-25 12:22:26 +0100

    refactor: use schema globals simplify gql schema

--- server/src/graphql/appliances/index.js
+++ server/src/graphql/appliances/index.js
@@ -1,7 +1,9 @@
+const path = require("path");
 const directives = require("./directives/index.js");
 const scalars = require("./scalars/index.js");
 
 module.exports = {
     directives,
-    scalars
+    scalars,
+    schemaGlobals: path.join(__dirname, "./Globals.gql")
 };
