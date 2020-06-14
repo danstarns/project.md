0e5c11643e5c936707a21fd0031c54c7d72ed70f
Daniel Starns <danielstarns@hotmail.com>
2020-06-14 10:35:58 +0100

    feat: lockdown project query

--- server/src/graphql/nodes/Project/Query/project.js
+++ server/src/graphql/nodes/Project/Query/project.js
@@ -1,7 +1,33 @@
-const { Project } = require("../../../../models/index.js");
+const { Project, Organization } = require("../../../../models/index.js");
 
-function project(root, { id }) {
-    return Project.findById(id);
+async function project(root, { id }, ctx) {
+    const foundProject = await Project.findById(id);
+
+    if (!foundProject) {
+        return null;
+    }
+
+    if (foundProject.private && ctx.user !== foundProject.creator.toString()) {
+        throw new Error("Forbidden");
+    }
+
+    if (foundProject.organization) {
+        const organization = await Organization.findById(
+            foundProject.organization
+        );
+
+        const includedUsers = [
+            organization.creator,
+            ...organization.admins,
+            ...organization.users
+        ];
+
+        if (!includedUsers.includes(ctx.user)) {
+            throw new Error("Forbidden");
+        }
+    }
+
+    return foundProject;
 }
 
 module.exports = project;
