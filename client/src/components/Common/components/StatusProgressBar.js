be8ec8b0ae17f9ae48c0587dab4fd251774dc4d3
Daniel Starns <danielstarns@hotmail.com>
2020-06-14 18:42:45 +0100

    feat: change color of progress bar at diff stage

--- client/src/components/Common/components/StatusProgressBar.js
+++ client/src/components/Common/components/StatusProgressBar.js
@@ -3,21 +3,25 @@ import { ProgressBar } from "react-bootstrap";
 
 function StatusProgressBar(props) {
   let now;
+  let color;
 
   // eslint-disable-next-line default-case
   switch (props.status) {
     case "Todo":
       now = 33;
+      color = "warning";
       break;
     case "InProgress":
       now = 66;
+      color = "primary";
       break;
     case "Done":
       now = 100;
+      color = "success";
       break;
   }
 
-  return <ProgressBar variant="primary" now={now} label={props.status} />;
+  return <ProgressBar variant={color} now={now} label={props.status} />;
 }
 
 export default StatusProgressBar;
