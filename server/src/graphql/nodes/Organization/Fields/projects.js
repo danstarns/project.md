8a993d216e210aac83ed354bfdb23b9522761210
Daniel Starns <danielstarns@hotmail.com>
2020-06-27 09:24:08 +0100

    fix: filter org projects

--- server/src/graphql/nodes/Organization/Fields/projects.js
+++ server/src/graphql/nodes/Organization/Fields/projects.js
@@ -1,21 +1,32 @@
 const { Project } = require("../../../../models/index.js");
 
-async function projects(root, { input: { page, limit, search, sort } }) {
+async function projects(
+    root,
+    { input: { page, limit, search, sort, user } },
+    ctx
+) {
     const regexMatch = { $regex: new RegExp(search), $options: ["i", "g"] };
 
-    const { docs, hasNextPage } = await Project.paginate(
-        {
-            organization: root._id,
-            ...(search
-                ? { $or: [{ name: regexMatch }, { tagline: regexMatch }] }
-                : {})
-        },
-        {
-            page,
-            limit,
-            sort: { createdAt: sort }
-        }
-    );
+    const query = {
+        organization: root._id,
+        ...(search
+            ? { $or: [{ name: regexMatch }, { tagline: regexMatch }] }
+            : {})
+    };
+
+    if (user) {
+        delete query.organization;
+
+        query.$and = [{ organization: root._id }, { creator: ctx.user }];
+    } else {
+        query.private = false;
+    }
+
+    const { docs, hasNextPage } = await Project.paginate(query, {
+        page,
+        limit,
+        sort: { createdAt: sort }
+    });
 
     return {
         projects: docs,
