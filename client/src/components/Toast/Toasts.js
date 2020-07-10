54b8e0d5f77ae28d6985c7c6f31ea9b7fee67331
Daniel Starns <danielstarns@hotmail.com>
2020-07-10 18:21:08 +0100

    fix: toasts to stack

--- client/src/components/Toast/Toasts.js
+++ client/src/components/Toast/Toasts.js
@@ -6,16 +6,12 @@ function Toasts() {
   const { toasts, removeToast } = useContext(ToastContext.Context);
 
   return (
-    <div
-      aria-live="polite"
-      aria-atomic="true"
-      style={{
-        position: "relative"
-      }}
-    >
-      {toasts.length
-        ? toasts.map(t => <Toast toast={t} removeToast={removeToast} />)
-        : null}
+    <div aria-live="polite" aria-atomic="true" className="toasts-outer">
+      <div className="toasts-inner">
+        {toasts.length
+          ? toasts.map(t => <Toast toast={t} removeToast={removeToast} />)
+          : null}
+      </div>
     </div>
   );
 }
