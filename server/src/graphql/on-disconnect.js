a52f7e19bcfa5adbf6acc6c9c6c2b80840680c90
Daniel Starns <danielstarns@hotmail.com>
2020-07-12 19:19:45 +0100

    fix: default subscriptions to empty array

--- server/src/graphql/on-disconnect.js
+++ server/src/graphql/on-disconnect.js
@@ -1,11 +1,11 @@
 const redis = require("../redis.js");
 
 async function onDisconnect(ws, ctx) {
-    const context = await ctx.initPromise;
+    const { user, subscriptions = [] } = await ctx.initPromise;
 
     await Promise.all([
-        redis.dbs.onlineUsers.del(context.user),
-        ...context.subscriptions.map((x) => x.unsubscribe())
+        redis.dbs.onlineUsers.del(user),
+        ...subscriptions.map((x) => x.unsubscribe())
     ]);
 }
 
