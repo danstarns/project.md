b66475d962e2725d3660810733275be9e60857bf
Daniel Starns <danielstarns@hotmail.com>
2020-07-19 09:42:38 +0100

    feat: missing index

--- server/src/graphql/nodes/Organization/Mutation/index.js
+++ server/src/graphql/nodes/Organization/Mutation/index.js
@@ -2,10 +2,14 @@ const createOrganization = require("./create-organization.js");
 const editOrganization = require("./edit-organization.js");
 const inviteUserOrganization = require("./invite-user-organization.js");
 const inviteUserOrganizationCallback = require("./invite-user-organization-callback.js");
+const assignAdminOrganization = require("./assign-admin-organization.js");
+const revokeAdminOrganization = require("./revoke-admin-organization.js");
 
 module.exports = {
     createOrganization,
     editOrganization,
     inviteUserOrganization,
-    inviteUserOrganizationCallback
+    inviteUserOrganizationCallback,
+    assignAdminOrganization,
+    revokeAdminOrganization
 };
