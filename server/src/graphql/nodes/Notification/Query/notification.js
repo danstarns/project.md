34ae8119581d806b7ca604a2ea4ce9e61709582e
Daniel Starns <danielstarns@hotmail.com>
2020-07-19 09:42:29 +0100

    fix: add seen to notification when query

--- server/src/graphql/nodes/Notification/Query/notification.js
+++ server/src/graphql/nodes/Notification/Query/notification.js
@@ -1,7 +1,11 @@
 const { Notification } = require("../../../../models/index.js");
 
 function notification(root, { id }) {
-    return Notification.findById(id);
+    return Notification.findByIdAndUpdate(
+        id,
+        { $set: { seen: true } },
+        { new: true }
+    );
 }
 
 module.exports = notification;
