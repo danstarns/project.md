6c744f8ef3882ae544e9a9cf234ee4b4b08fb8e0
Daniel Starns <danielstarns@hotmail.com>
2020-07-20 20:48:23 +0100

    feat: add dropdown for user profile page

--- client/src/components/User/components/ProfilePic.js
+++ client/src/components/User/components/ProfilePic.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Dropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../User.css";
 
 function ProfilePic(props) {
   return (
-    <Card className="p-3">
-      <Card className="profile-pic mb-3 mx-auto">
+    <Card className="d-flex justify-content-between align-items-center p-3">
+      <Card className="profile-pic mb-3">
         {props.profile.profilePic ? (
           <img
             className="profile-pic"
@@ -20,22 +20,27 @@ function ProfilePic(props) {
         )}
       </Card>
       <p className="pl-3 pr-3 profile-box-text">
-        <FontAwesomeIcon icon="user-tag" />
+        <FontAwesomeIcon icon="user-tag" size="1x" />
         <span className="ml-1">{props.profile.username}</span>
       </p>
       <p className="pl-3 pr-3 profile-box-text">
-        <FontAwesomeIcon icon="at" />
+        <FontAwesomeIcon icon="at" size="1x" />
         <span className="ml-1">{props.profile.email}</span>
       </p>
-      {props.profile.isRequester && (
-        <Button
-          className="m-0 mb-3"
-          variant="primary"
-          onClick={props.setEditUserModal}
-        >
-          Edit
-        </Button>
-      )}
+      <div className="d-flex justify-content-between align-items-center pt-2">
+        {props.profile.isRequester && (
+          <Dropdown className="m-1">
+            <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic">
+              <FontAwesomeIcon icon="cog" size="1x" className="mr-2" />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={props.setEditUserModal}>
+                Edit
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        )}
+      </div>
     </Card>
   );
 }
