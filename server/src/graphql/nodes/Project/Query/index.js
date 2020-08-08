8c08f3c2a0923cf23c207afb3245c900a634083f
Daniel Starns <danielstarns@hotmail.com>
2020-08-08 12:17:27 +0100

    feat: add projects query

--- server/src/graphql/nodes/Project/Query/index.js
+++ server/src/graphql/nodes/Project/Query/index.js
@@ -1,9 +1,11 @@
 const project = require("./project.js");
 const userProjects = require("./user-projects.js");
 const publicProjects = require("./public-projects.js");
+const projects = require("./projects.js");
 
 module.exports = {
     project,
     userProjects,
-    publicProjects
+    publicProjects,
+    projects
 };
---
+++ server/src/graphql/nodes/Project/Query/projects.js
@@ -0,0 +1,49 @@
+const { Project, Organization } = require("../../../../models/index.js");
+
+async function projects(root, { input: { page, limit, sort, search } }, ctx) {
+    const regexMatch = { $regex: new RegExp(search), $options: ["i", "g"] };
+
+    const query = {
+        private: false,
+        $and: [
+            ...(search
+                ? { $or: [{ name: regexMatch }, { tagline: regexMatch }] }
+                : {})
+        ]
+    };
+
+    if (ctx.user) {
+        const adminOfOrgs = await Organization.find({
+            $or: [
+                { users: ctx.user },
+                { admins: ctx.user },
+                { creator: ctx.user }
+            ]
+        }).select("_id");
+
+        delete query.private;
+
+        query.$and.push({
+            $or: [
+                { creator: ctx.user },
+                { users: ctx.user },
+                { organization: { $in: adminOfOrgs.map((x) => x._id) } }
+            ]
+        });
+    }
+
+    const { docs, hasNextPage } = await Project.paginate(query, {
+        page,
+        limit,
+        sort: { createdAt: sort }
+    });
+
+    return {
+        hasNextPage,
+        data: {
+            projects: docs
+        }
+    };
+}
+
+module.exports = projects;
