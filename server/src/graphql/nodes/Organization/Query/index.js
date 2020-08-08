6c7a6a3e7c6a354a0d1b53fd717fa730a9f33095
Daniel Starns <danielstarns@hotmail.com>
2020-08-08 12:05:13 +0100

    feat: add organizations query

--- server/src/graphql/nodes/Organization/Query/index.js
+++ server/src/graphql/nodes/Organization/Query/index.js
@@ -1,9 +1,11 @@
 const organization = require("./organization.js");
 const publicOrganizations = require("./public-organizations.js");
 const userOrganizations = require("./user-organizations.js");
+const organizations = require("./organizations.js");
 
 module.exports = {
     organization,
     publicOrganizations,
-    userOrganizations
+    userOrganizations,
+    organizations
 };
---
+++ server/src/graphql/nodes/Organization/Query/organizations.js
@@ -0,0 +1,40 @@
+const { Organization } = require("../../../../models/index.js");
+
+async function organizations(
+    root,
+    { input: { page, limit, sort, search } },
+    ctx
+) {
+    const regexMatch = { $regex: new RegExp(search), $options: ["i", "g"] };
+
+    const query = {
+        $and: [
+            ...(search
+                ? [{ $or: [{ name: regexMatch }, { tagline: regexMatch }] }]
+                : [])
+        ]
+    };
+
+    if (ctx.user) {
+        query.$and.push({
+            $or: [
+                { creator: ctx.user },
+                { admins: ctx.user },
+                { users: ctx.user }
+            ]
+        });
+    }
+
+    const { docs, hasNextPage } = await Organization.paginate(query, {
+        page,
+        limit,
+        sort: { createdAt: sort }
+    });
+
+    return {
+        hasNextPage,
+        organizations: docs
+    };
+}
+
+module.exports = organizations;
